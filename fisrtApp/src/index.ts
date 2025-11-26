1b410aa14b9e51e914ce7ac67e16fc9c2fe4afd4
pathathai <pathathai.n@gmail.com>
2025-11-26 21:04:38 +0700

    add style to h1

--- fisrtApp/src/index.ts
+++ fisrtApp/src/index.ts
@@ -8,6 +8,7 @@ app.get("/", (req: Request, res: Response) => {
   res.writeHead(200, { "Content-Type": "text/html" });
   res.write("<meta charset='UTF-8'>");
   res.write("<title>Express with TypeScript</title>");
+  res.write("<style> h1 { color: #3498db; } h2 { color: #2ecc71; } </style>");
   res.end("<h1>Hello World!.</h1> <h2>Welcome to Express with TypeScript.</h2>");
 });
 
