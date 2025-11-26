1efe40754c6fc1dedd9a11391ccc6f053ec475f9
pathathai <pathathai.n@gmail.com>
2025-11-26 21:52:55 +0700

    remove res.write

--- fisrtApp/src/index.ts
+++ fisrtApp/src/index.ts
@@ -6,10 +6,7 @@ const port = 3000;
 app.get("/", (req: Request, res: Response) => {
   // res.status(200).json({message: 'Hello, World!'});
   res.writeHead(200, { "Content-Type": "text/html" });
-  res.write("<meta charset='UTF-8'>");
-  res.write("<title>Express with TypeScript</title>");
-  res.write("<style> h1 { color: #3498db; } h2 { color: #2ecc71; } </style>");
-  res.end("<h1>Hello World!.</h1> <h2>Welcome to Express with TypeScript.</h2>");
+  res.end("<h1>Hello World!.</h1>");
 });
 
 app.listen(port, () => {
