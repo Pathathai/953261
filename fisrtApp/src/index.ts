4384bcc68be15091d60dd2852c28a1636cae67ca
pathathai <pathathai.n@gmail.com>
2025-11-26 20:51:24 +0700

    add res.write

--- fisrtApp/src/index.ts
+++ fisrtApp/src/index.ts
@@ -6,6 +6,7 @@ const port = 3000;
 app.get("/", (req: Request, res: Response) => {
   // res.status(200).json({message: 'Hello, World!'});
   res.writeHead(200, { "Content-Type": "text/html" });
+  res.write("<meta charset='UTF-8'>");
   res.end("<h1>Hello World!.</h1> <h2>Welcome to Express with TypeScript.</h2>");
 });
 
