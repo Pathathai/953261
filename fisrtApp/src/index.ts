15a5668d0e50ab436e8c03bb8be6859e5ee08ace
pathathai <pathathai.n@gmail.com>
2025-12-10 11:10:35 +0700

    commit LAB01: Initial commit with project setup and assets

--- fisrtApp/src/index.ts
+++ fisrtApp/src/index.ts
@@ -6,7 +6,7 @@ const port = 3000;
 app.get("/", (req: Request, res: Response) => {
   // res.status(200).json({message: 'Hello, World!'});
   res.writeHead(200, { "Content-Type": "text/html" });
-  res.end("<h1>Hello World!.</h1>");
+  res.end("<h1>Hello from Express + TypeScript!</h1>");
 });
 
 app.listen(port, () => {
